61a2ac8983fc663047f25e60fa8362f9f8f67db6
Vladimir Terehov <terekhov_v_a@mail.ru>
2022-07-11 14:58:33 +0300

    Создано приложение и компоненты

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <HeaderComponent />
+  <TaskComponent />
+  <TaskListComponent />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HeaderComponent from "@/components/HeaderComponent.vue";
+import TaskComponent from "@/components/TaskComponent.vue";
+import TaskListComponent from "@/components/TaskListComponent.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    HeaderComponent,
+    TaskComponent,
+    TaskListComponent,
   },
 };
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
